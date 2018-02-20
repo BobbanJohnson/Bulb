1d103e5b5d621bdb43ec01454494b5472c23ba6b
Bobban <bobbanjohnson@gmail.com>
2018-02-20 14:07:01 +0100

    Changed rotation and add post processing 2
    Yeah that

--- Conductive/Assets/Scripts/PlayerMovement.cs
+++ Conductive/Assets/Scripts/PlayerMovement.cs
@@ -5,126 +5,57 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour {
 
     // Movement variables
-    [SerializeField] private float movementSpeed;
-    [SerializeField] private float gravity;
-    [SerializeField] private float antiBumpFactor;
-    public int movementHorizontal, movementVertical = 0;
+    [SerializeField] Transform _rotationTransform;
+    [SerializeField] float _movementSpeed;
+    [SerializeField] float _gravity;
 
     // Character references
-    private Transform playerGraphics;
-    private Vector3 moveDirection = Vector3.zero;
-    private CharacterController characterController;
+    CharacterController _characterController;
 
     // Rotation variables and references
-    [SerializeField] private float rotationSpeed;
-    private Transform target;
-    private Quaternion targetRotation;
-    private float rotationTime;
-    private bool rotating = false;
+    [SerializeField] float _rotationSpeed;
+    Quaternion _targetRotation;
 
-
-    void Start()
+    void Awake()
     {
-        playerGraphics = transform.GetChild(0);
-        target = transform.GetChild(1);
-        characterController = GetComponent<CharacterController>();
+        _characterController = GetComponent<CharacterController>();
     }
 
     void Update()
     {
-        // Create a movement vector to move charactercontroller
-        if (characterController.isGrounded)
-        {
-            moveDirection = new Vector3(Input.GetAxis("Horizontal"), -antiBumpFactor, Input.GetAxis("Vertical"));
-            moveDirection = transform.TransformDirection(moveDirection);
-            moveDirection *= movementSpeed;
-        }
+        ApplyGravity();
 
-        moveDirection.y -= gravity * Time.deltaTime;
-        characterController.Move(moveDirection * Time.deltaTime);
-
-        // Check if the player is moving up or down;
-        if (Input.GetKeyDown(KeyCode.S))
-            movementVertical = -5;
-
-        if (Input.GetKeyUp(KeyCode.S))
-            movementVertical = 0;
-
-        if (Input.GetKeyDown(KeyCode.W))
-            movementVertical = 5;
-
-        if (Input.GetKeyUp(KeyCode.W))
-            movementVertical = 0;
-
-         //Check if the player is moving up or down;
-        if (Input.GetKeyDown(KeyCode.A))
-            movementHorizontal = -5;
-
-        if (Input.GetKeyUp(KeyCode.A))
-            movementHorizontal = 0;
-
-        if (Input.GetKeyDown(KeyCode.D))
-            movementHorizontal = 5;
-
-        if (Input.GetKeyUp(KeyCode.D))
-            movementHorizontal = 0;
-
-        MovePositionToLookAt(new Vector3(movementHorizontal, 0, movementVertical));
-
-        /*
-        // Move rotation transform depending on input
-        /// Vertical movement rotation
-        if (Input.GetKeyDown(KeyCode.S))
-            MovePositionToLookAt(new Vector3(0, 0, -5));
-
-        else if (Input.GetKeyDown(KeyCode.W))
-            MovePositionToLookAt(new Vector3(0, 0, 5));
-
-        /// Horizontal movement rotation
-        if (Input.GetKeyDown(KeyCode.A))
-            MovePositionToLookAt(new Vector3(-5, 0, 0));
-
-        else if (Input.GetKeyDown(KeyCode.D))
-            MovePositionToLookAt(new Vector3(5, 0, 0));
-        
-
-
-
-        /// Upper diagnoal movement rotation
-        if (Input.GetKeyDown(KeyCode.W) && (Input.GetKeyDown(KeyCode.A)))
-            MovePositionToLookAt(new Vector3(-5, 0, 5));
-
-        else if (Input.GetKeyDown(KeyCode.W) && (Input.GetKeyDown(KeyCode.D)))
-            MovePositionToLookAt(new Vector3(5, 0, 5));
-
-        /// Lower diagnoal movement rotation
-        if (Input.GetKeyDown(KeyCode.S) && (Input.GetKeyDown(KeyCode.A)))
-            MovePositionToLookAt(new Vector3(-5, 0, -5));
-
-        else if (Input.GetKeyDown(KeyCode.S) && (Input.GetKeyDown(KeyCode.D)))
-            MovePositionToLookAt(new Vector3(5, 0, -5));
-            */
-
-
-        // Rotate the player towards a moving transform
-        if (rotating)
-        {
-            rotationTime += Time.deltaTime * rotationSpeed;
-            playerGraphics.rotation = Quaternion.Lerp(playerGraphics.rotation, targetRotation, rotationTime);
-            if (rotationTime > 1)
-            {
-                rotating = false;
-            }
-        }
+        Vector3 moveDirection = GetMoveInput();
+        HandleMovement(moveDirection);
+        HandleRotation(moveDirection);
     }
-    
-    // Rotation to move the character
-    void MovePositionToLookAt(Vector3 targetPosition)
+
+    Vector3 GetMoveInput()
     {
-        target.position = transform.position + targetPosition;
-        Vector3 relativePosition = target.position - playerGraphics.position;
-        targetRotation = Quaternion.LookRotation(relativePosition);
-        rotating = true;
-        rotationTime = 0;
+        if (_characterController.isGrounded)
+            return new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+
+        return Vector3.zero;
     }
+
+    void ApplyGravity()
+    {
+        _characterController.Move(Vector3.up * -_gravity * Time.deltaTime);
+    }
+
+    void HandleMovement(Vector3 inMoveDirection)
+    {
+        _characterController.Move(inMoveDirection * Time.deltaTime * _movementSpeed);
+    }
+
+    void HandleRotation(Vector3 inMoveDirection)
+    {
+        if (inMoveDirection == Vector3.zero)
+            return;
+
+        Quaternion currentRotation = _rotationTransform.rotation;
+        _targetRotation = Quaternion.LookRotation(inMoveDirection.normalized, Vector3.up);
+        _rotationTransform.rotation = Quaternion.RotateTowards(currentRotation, _targetRotation, _rotationSpeed * Time.deltaTime);
+    }
+
 }
